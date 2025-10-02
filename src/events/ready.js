34ed76f0b8c5f04a1a27c76f99a73b1609fbe98f
Fernando Augusto <nando.aju.2001@gmail.com>
2025-10-01 23:35:53 -0300

    primeiro teste

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,5 @@
 const { Events } = require('discord.js');
-const { postarServicosAutomatico } = require('../commands/postarServicos');
+// const { postarServicosAutomatico } = require('../commands/postarServicos');
 
 module.exports = {
     name: Events.ClientReady,
@@ -7,7 +7,9 @@ module.exports = {
     async execute(client) {
         console.log(`🤖 Bot ANTI-DUPLICAÇÃO online: ${client.user.tag}!`);
         client.user.setActivity('🛒 Sistema Único v2.2', { type: 'PLAYING' });
-
+        
+        // COMENTAR ESTAS LINHAS PARA PARAR O REENVIO:
+        /*
         console.log('🚀 Iniciando configuração automática dos servidores...');
         for (const [guildId, guild] of client.guilds.cache) {
             try {
@@ -17,6 +19,8 @@ module.exports = {
                 console.error(`❌ Erro ao configurar o servidor ${guild.name}:`, error);
             }
         }
-        console.log('🎉 Sistema totalmente operacional!');
+        */
+        
+        console.log('🎉 Bot operacional sem repostagem automática!');
     },
 };
